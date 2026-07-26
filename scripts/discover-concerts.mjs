6f64eb27af31013160b2b72e5408e2ba7823a01a
mr-tanq <errtanq9@gmail.com>
2026-07-27 00:05:33 +0200

    Update discover-concerts.mjs

--- scripts/discover-concerts.mjs
+++ scripts/discover-concerts.mjs
@@ -52,7 +52,7 @@ async function getLastfmWeightedArtists() {
     return [];
   }
 
-  const topUrl = `https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${user}&api_key=${key}&format=json&period=6month&limit=200`;
+  const topUrl = `https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${user}&api_key=${key}&format=json&period=overall&limit=200`;
   const recentUrl = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&api_key=${key}&format=json&limit=200`;
 
   const [topRes, recentRes] = await Promise.all([fetchWithRetry(topUrl), fetchWithRetry(recentUrl)]);
