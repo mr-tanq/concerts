612cd9f9f60211db7eee4ceae3bde2a7c362782a
mr-tanq <errtanq9@gmail.com>
2026-08-04 17:21:27 +0200

    Update mirror.js

--- js/mirror.js
+++ js/mirror.js
@@ -272,6 +272,7 @@ async function renderNowPlaying(body, state) {
 
     getSyncedLyrics({ artist: state.primaryArtist, track: state.track, album: state.album, durationSec: state.durationMs / 1000 })
       .then(({ lines }) => { currentLyricLines = lines; updateLyricLine(body, state.progressMs); });
+  }
 
   const fill = body.querySelector("#mirror-progress-fill");
   if (fill && state.durationMs) fill.style.width = `${Math.min(100, (state.progressMs / state.durationMs) * 100)}%`;
